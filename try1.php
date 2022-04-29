cadae084dd15ef0f388095f8ffd5314e41e15910
Fitrivv <fitriwlndr18@gmail.com>
2022-04-29 10:54:47 +0700

    tambahan file lagi

---
+++ try1.php
@@ -0,0 +1,15 @@
+<?php
+
+//$nama = "fitri";
+//echo "Halo nama saya ".$nama;
+
+//echo "<br>";
+
+//$nama = "zahra";
+//echo "halo nama saya " .$nama;
+
+
+$nama = array("fitri", "zahra", "januri", "dewi");
+
+echo "hi, nama saya " .$nama[0]." saya punya adik namanya " .$nama[1]. " ayah saya " .$nama[2]. " ibu saya namanya " .$nama[3];
+?> 
